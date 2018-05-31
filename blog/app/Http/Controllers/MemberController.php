f4daca6c65104dacaf116e0a05f542707a426573
jacobhsu <jacob.hsu.tw@gmail.com>
2018-05-31 22:21:06 +0800

    member/info.blade.php

--- blog/app/Http/Controllers/MemberController.php
+++ blog/app/Http/Controllers/MemberController.php
@@ -6,7 +6,11 @@ class MemberController extends Controller
 {
     public function info($id)
     {
-    	return 'member-info-id-'.$id;
+    	//return 'member-info-id-'.$id;
     	//return route('memberInfo');
+    	//return view('member-info');
+    	return view('member/info',[
+    		'name' => 'jacobhsu'
+    	]);
     }
 }
